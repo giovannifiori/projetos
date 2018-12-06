895ec4dc22e700488b481ddfb2a2da5dc72b2b5c
Giovanni Fiori <giovannicfiori@gmail.com>
2018-12-06 17:39:00 -0400

    Adicionado campos na tela de 'view' de despesa para quando do tipo passagem ou diária

--- backend/views/despesa/view.php
+++ backend/views/despesa/view.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use backend\models\Despesa;
+use backend\models\DespesaPassagem;
+use backend\models\DespesaDiaria;
 use backend\models\Fornecedor;
 use backend\models\Beneficiario;
 use backend\models\Item;
@@ -14,6 +16,109 @@ $this->title = "Despesa " . $despesaModel->id;
 $this->params['breadcrumbs'][] = ['label' => 'Despesas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
+
+$viewAttributes = [
+    'id',
+    [
+        'attribute' => 'valor_unitario',
+        'value' => function($model){
+            return "R$" . ($model->valor_unitario ? $model->valor_unitario : "0");
+        }
+    ],
+    'qtde',
+    [
+        'label' => 'Valor total',
+        'value' => function($model){
+            return "R$" . ($model->valor_unitario * $model->qtde);
+        }
+    ],
+    [
+        'attribute' => 'tipo_desp',
+        'label' => 'Tipo',
+        'value' => function($model) {
+            $t = $model->getTiposDespesa();
+            return isset($t) ? $t[$model->tipo_desp] : "-";
+        }
+    ],
+    [
+        'attribute' => 'status',
+        'value' => function($model){
+            return $model->getStatus()[$model->status];
+        }
+    ],
+    'data_emissao_NF',
+    'pendencias:ntext',
+    'numero_cheque',
+    'data_pgto',
+    'nf_recibo',
+    'objetivo:ntext',
+    [
+        'attribute' => 'id_beneficiario',
+        'label' => 'Beneficiário',
+        'value' => function($model) {
+            $b = Beneficiario::findOne($model->id_beneficiario);
+            return isset($b) ? $b->nome . " - " . $b->rg : "Beneficiário não registrado";
+        }
+    ],
+    [
+        'attribute' => 'id_fornecedor',
+        'label' => 'Fornecedor',
+        'value' => function($model) {
+            $f = Fornecedor::findOne($model->id_fornecedor);
+            return isset($f) ? $f->nome . " - " . $f->cpf_cnpj : "Fornecedor não registrado";
+        }
+    ],
+    [
+        'attribute' => 'id_item',
+        'label' => 'Item',
+        'value' => function($model) {
+            $item = Item::findOne($model->id_item);
+            return isset($item) ? $item->descricao : "Item não registrado";
+        }
+    ],
+];
+
+if(isset($despesapassagemModel) && !empty($despesapassagemModel)){
+    array_push($viewAttributes, [
+        'label' => 'Data/Hora de ida',
+        'value' => function($model){
+            return DespesaPassagem::find()->where(['id_despesa' => $model->id])->one()->data_hora_ida;
+        }
+    ], [
+        'label' => 'Data/Hora de volta',
+        'value' => function($model){
+            return DespesaPassagem::find()->where(['id_despesa' => $model->id])->one()->data_hora_volta;
+        }
+    ], [
+        'label' => 'Localizador',
+        'value' => function($model){
+            return DespesaPassagem::find()->where(['id_despesa' => $model->id])->one()->localizador;
+        }
+    ], [
+        'label' => 'Destino',
+        'value' => function($model){
+            return DespesaPassagem::find()->where(['id_despesa' => $model->id])->one()->destino;
+        }
+    ]);
+}elseif (isset($despesadiariaModel) && !empty($despesadiariaModel)){
+    array_push($viewAttributes, [
+            'label' => 'Data/Hora de ida',
+            'value' => function($model){
+                return DespesaDiaria::find()->where(['id_despesa' => $model->id])->one()->data_hora_ida;
+            }
+        ], [
+            'label' => 'Data/Hora de volta',
+            'value' => function($model){
+                return DespesaDiaria::find()->where(['id_despesa' => $model->id])->one()->data_hora_volta;
+            }
+        ], [
+            'label' => 'Destino',
+            'value' => function($model){
+                return DespesaDiaria::find()->where(['id_despesa' => $model->id])->one()->destino;
+            }
+        ]);
+}
+
 ?>
 <div class="despesa-view">
 
@@ -31,59 +136,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= DetailView::widget([
         'model' => $despesaModel,
-        'attributes' => [
-            'id',
-            [
-                'attribute' => 'valor_unitario',
-                'value' => function($model){
-                    return "R$" . ($model->valor_unitario ? $model->valor_unitario : "0");
-                }
-            ],
-            'qtde',
-            [
-                'label' => 'Valor total',
-                'value' => function($model){
-                    return "R$" . ($model->valor_unitario * $model->qtde);
-                }
-            ],
-            'tipo_desp',
-            [
-                'attribute' => 'status',
-                'value' => function($model){
-                    return $model->getStatus()[$model->status];
-                }
-            ],
-            'data_emissao_NF',
-            'pendencias:ntext',
-            'numero_cheque',
-            'data_pgto',
-            'nf_recibo',
-            'objetivo:ntext',
-            [
-                'attribute' => 'id_beneficiario',
-                'label' => 'Beneficiário',
-                'value' => function($model) {
-                    $b = Beneficiario::findOne($model->id_beneficiario);
-                    return isset($b) ? $b->nome . " - " . $b->rg : "Beneficiário não registrado";
-                }
-            ],
-            [
-                'attribute' => 'id_fornecedor',
-                'label' => 'Fornecedor',
-                'value' => function($model) {
-                    $f = Fornecedor::findOne($model->id_fornecedor);
-                    return isset($f) ? $f->nome . " - " . $f->cpf_cnpj : "Fornecedor não registrado";
-                }
-            ],
-            [
-                'attribute' => 'id_item',
-                'label' => 'Item',
-                'value' => function($model) {
-                    $item = Item::findOne($model->id_item);
-                    return isset($item) ? $item->descricao : "Item não registrado";
-                }
-            ],
-        ],
+        'attributes' => $viewAttributes,
     ]) ?>
 
 </div>
